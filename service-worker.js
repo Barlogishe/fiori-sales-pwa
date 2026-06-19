908e4e6ea258fe845b8f83c95aa2a3071ae75ec0
Barlogishe <mickhail2000@gmail.com>
2026-06-19 19:19:52 +0300

    Added API version log

--- service-worker.js
+++ service-worker.js
@@ -16,6 +16,21 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      ))
+      .then(() => self.clients.claim())
   );
 
 });
